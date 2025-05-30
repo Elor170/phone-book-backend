191daaa59ccfb0284bdf1d8ba411f82dd99c63ab
Elor Ifrach <elor170@gmail.com>
2025-05-30 17:08:36 +0300

    deploy updated version

--- Services/database.js
+++ Services/database.js
@@ -13,19 +13,33 @@ mongoose.connect(MONGODB_URI)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        require: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function (v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+    }
 })
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
-  })
+})
 
 const Person = mongoose.model('Person', personSchema)
 
 
-export  { Person }
+export { Person }
--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 import express from 'express'
 import morgan from 'morgan'
 import { Person } from './Services/database.js'
+import 'dotenv/config'
+const { PORT } = process.env
 
-let notes = [
-  {
-    "id": "1",
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": "3",
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
-const PORT = 3001
 const app = express();
 app.use(express.json())
 app.use(morgan(function (tokens, req, res) {
@@ -40,62 +18,75 @@ app.use(morgan(function (tokens, req, res) {
 }))
 app.use(express.static('dist'))
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
   Person.find({})
-    .then(people => {
-      response.json(people)
+    .then(people => response.json(people))
+    .catch(err => next(err))
+})
+
+app.get('/info', (request, response, next) => {
+  Person.find({})
+    .then(result => {
+      const data = `Phone book has info for ${result.length} people\n\n${new Date()}`
+      response.contentType('text/plain')
+      response.send(data)
     })
+    .catch(err => next(err))
+
 })
 
-app.get('/info', (request, response) => {
-  const data = `Phone book has info for ${notes.length} people\n\n${new Date()}`
-  response.contentType('text/plain')
-  response.send(data)
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const { id } = request.params
-  const note = notes.find(note => note.id === id)
-
-  if (note)
-    response.json(note)
-  else
-    response.status(404).end()
-})
-
-app.delete('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   const { id } = request.params
 
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Person.findById(id)
+    .then(result => {
+      if (result)
+        response.json(result)
+      else
+        response.status(404).end()
+    })
+    .catch(err => next(err))
 })
 
-app.post('/api/persons/', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
+  const { id } = request.params
+
+  Person.findByIdAndDelete(id)
+    .then(res =>
+      response.status(204).end()
+    )
+    .catch(err => next(err))
+})
+
+app.post('/api/persons/', (request, response, next) => {
   const { name, number } = request.body
 
   if (!name || !number)
     response.status(400).json({ error: 'Name or number is missing' })
 
-  // TODO: add check if person already exists
-  // else if (notes.some(note => note.name === name)) {
-  //   response.status(400).json({ error: 'Name must be unique' })
-  // }
 
   else {
-    const newPerson = new Person({
-      name: name,
-      number: number
-    })
-    newPerson.save()
-    .then( res =>
-      response.json(res)
-    )
+    Person.findOne({ name })
+      .then(result => {
+        if (result)
+          response.status(400).json({ error: 'Name must be unique' })
+
+        else {
+          const newPerson = new Person({
+            name: name,
+            number: number
+          })
+          newPerson.save()
+            .then(res => response.json(res))
+            .catch(err => next(err))
+        }
+      })
+      .catch(err => next(err))
   }
 
 })
 
-app.put('/api/persons/:id', (req, res) => {
+app.put('/api/persons/:id', (req, res, next) => {
   const { id } = req.params
   const newData = req.body
   const { name, number } = newData
@@ -103,14 +94,37 @@ app.put('/api/persons/:id', (req, res) => {
   if (!name || !number)
     response.status(400).json({ error: 'Name or number is missing' })
 
-  else if (!notes.some(note => note.id === id))
-    res.status(404).json({ error: 'Id not found' })
-
   else {
-    notes = notes.map(note => note.id === id ? newData : note)
-    res.json(newData)
+    Person.findOneAndReplace({ _id: id }, { name, number }, { new: true, runValidators: true })
+      .then(result => {
+        if (result)
+          res.json(result)
+        else
+          res.status(404).json({ error: 'Id not found' })
+      })
+      .catch(err => next(err))
   }
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformed id' })
+  }
+  else if (error.name === 'ValidationError')
+    return response.status(400).json({error: error.message}) 
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
